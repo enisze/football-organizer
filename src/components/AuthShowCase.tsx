1c24107d96ed269025360eb7bbde814be99f0e03
Enis <eniszej@gmail.com>
2022-11-06 14:00:10 +0100

    ♻️

---
+++ src/components/AuthShowCase.tsx
@@ -0,0 +1,38 @@
+import { CssVarsProvider, Link, StyledEngineProvider } from "@mui/joy";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { trpc } from "../utils/trpc";
+import { Dashboard } from "./Dashboard";
+
+export const AuthShowcase: React.FC = () => {
+  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
+
+  const { data: sessionData } = useSession();
+
+  const { data: link } = trpc.gmail.test.useQuery();
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <CssVarsProvider>
+        <div className="flex flex-col items-center justify-center gap-2">
+          {sessionData && (
+            <p className="text-2xl text-blue-500">
+              Logged in as {sessionData?.user?.name}
+            </p>
+          )}
+          {secretMessage && (
+            <p className="text-2xl text-blue-500">{secretMessage}</p>
+          )}
+          <button
+            className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
+            onClick={sessionData ? () => signOut() : () => signIn()}
+          >
+            {sessionData ? "Sign out" : "Sign in"}
+          </button>
+          {sessionData && <Dashboard />}
+          <Link href={link}>Authorize gmail</Link>
+          <div>{link}</div>
+        </div>
+      </CssVarsProvider>
+    </StyledEngineProvider>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
-import { Link } from "@mui/joy";
-import { CssVarsProvider, StyledEngineProvider } from "@mui/joy/styles";
 import { type NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import { Dashboard } from "../components/Dashboard";
+import { AuthShowcase } from "../components/AuthShowCase";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
@@ -31,37 +28,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
-
-  const { data: sessionData } = useSession();
-
-  const { data: link } = trpc.gmail.test.useQuery();
-
-  return (
-    <StyledEngineProvider injectFirst>
-      <CssVarsProvider>
-        <div className="flex flex-col items-center justify-center gap-2">
-          {sessionData && (
-            <p className="text-2xl text-blue-500">
-              Logged in as {sessionData?.user?.name}
-            </p>
-          )}
-          {secretMessage && (
-            <p className="text-2xl text-blue-500">{secretMessage}</p>
-          )}
-          <button
-            className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
-            onClick={sessionData ? () => signOut() : () => signIn()}
-          >
-            {sessionData ? "Sign out" : "Sign in"}
-          </button>
-          {sessionData && <Dashboard />}
-          <Link href={link}>Authorize gmail</Link>
-          <div>{link}</div>
-        </div>
-      </CssVarsProvider>
-    </StyledEngineProvider>
-  );
-};
