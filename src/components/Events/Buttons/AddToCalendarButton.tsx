2d3ff3050302776a3ef10b25a6fc614bbe31f27e
Enis <eniszej@gmail.com>
2022-11-13 01:11:31 +0100

    ✨ Kalendars

--- src/components/Events/Buttons/AddToCalendarButton.tsx
+++ src/components/Events/Buttons/AddToCalendarButton.tsx
@@ -1,12 +1,21 @@
-import { Button } from "@mui/joy";
+import {
+  Button,
+  Modal,
+  ModalClose,
+  ModalDialog,
+  Sheet,
+  Typography,
+} from "@mui/joy";
 import type { Event } from "@prisma/client";
 import type { CalendarOptions } from "datebook";
-import { ICalendar } from "datebook";
+import { GoogleCalendar, ICalendar, OutlookCalendar } from "datebook";
 import type { FunctionComponent } from "react";
+import { useState } from "react";
 
 export const AddToCalendarButton: FunctionComponent<{ event: Event }> = ({
   event,
 }) => {
+  const [open, setOpen] = useState(false);
   const [startHours, startMinutes] = event.startTime.split(":");
   const [endHours, endMinutes] = event.endTime.split(":");
 
@@ -25,16 +34,78 @@ export const AddToCalendarButton: FunctionComponent<{ event: Event }> = ({
     end: end,
   };
   const icalendar = new ICalendar(options);
+  const googleCalendar = new GoogleCalendar(options);
+  const outlookCalendar = new OutlookCalendar(options);
+
+  const googleLink = googleCalendar.render();
+  const outlookLink = outlookCalendar.render();
 
   return (
-    <Button
-      variant="outlined"
-      color="primary"
-      onClick={() => {
-        icalendar.download("fussball.ics");
-      }}
-    >
-      Zum Kalender hinzufügen
-    </Button>
+    <>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Zum Kalender hinzufügen
+      </Button>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <ModalDialog
+          aria-labelledby="size-modal-title"
+          aria-describedby="size-modal-description"
+        >
+          <ModalClose variant="outlined" className="rounded shadow-md" />
+          <Typography
+            component="h2"
+            id="modal-title"
+            level="h4"
+            textColor="inherit"
+            fontWeight="lg"
+            mb={1}
+          >
+            Kalender wählen
+          </Typography>
+
+          <Sheet className="flex flex-col gap-y-2">
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => {
+                icalendar.download();
+              }}
+            >
+              ICal Kalendar
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => {
+                window.open(googleLink);
+              }}
+            >
+              Google Kalendar
+            </Button>
+
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => {
+                window.open(outlookLink);
+              }}
+            >
+              Outlook Kalendar
+            </Button>
+          </Sheet>
+        </ModalDialog>
+      </Modal>
+    </>
   );
 };
