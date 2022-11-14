200b1b7ad864fc40474c78cfcdb891b03f43de4f
Enis <eniszej@gmail.com>
2022-11-15 00:20:46 +0100

    ♻️

--- src/pages/oauth2callback.tsx
+++ src/pages/oauth2callback.tsx
@@ -1,17 +1,18 @@
 import { useRouter } from "next/router";
 import type { FunctionComponent } from "react";
+import { trpc } from "../utils/trpc";
 
 const Oauth2Callback: FunctionComponent = () => {
   const router = useRouter();
 
   const { code } = router.query;
 
-  // const { data } = trpc.gmail.getToken.useQuery(
-  //   { code: code as string },
-  //   { enabled: Boolean(code) }
-  // );
+  const { data } = trpc.gmail.getToken.useQuery(
+    { code: code as string },
+    { enabled: Boolean(code) }
+  );
 
-  return <div>{code}</div>;
+  return <div>{data?.refresh_token}</div>;
 };
 
 export default Oauth2Callback;
