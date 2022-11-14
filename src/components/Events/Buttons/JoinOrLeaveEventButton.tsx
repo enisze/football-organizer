c6f2d245758f8d6a2af699dcc41f32441da6e9f5
Enis <eniszej@gmail.com>
2022-11-14 23:42:24 +0100

    💄 ✨ Add leave confirmation

--- src/components/Events/Buttons/JoinOrLeaveEventButton.tsx
+++ src/components/Events/Buttons/JoinOrLeaveEventButton.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@mui/joy";
+import { Button, Modal, ModalClose, ModalDialog, Typography } from "@mui/joy";
 import { TRPCError } from "@trpc/server";
 import type { FunctionComponent } from "react";
+import { useState } from "react";
 import { trpc } from "../../../utils/trpc";
 
 export const JoinOrLeaveEventButton: FunctionComponent<{
   id: string;
   isUserParticipating: boolean;
 }> = ({ id, isUserParticipating }) => {
+  const [showLeaveModal, setShowLeaveModal] = useState(false);
   const trpcContext = trpc.useContext();
   const { mutateAsync: joinEvent } = trpc.event.join.useMutation({
     onSuccess: () => {
@@ -27,9 +29,15 @@ export const JoinOrLeaveEventButton: FunctionComponent<{
     },
   });
 
+  const { data: payment } = trpc.payment.getByEventId.useQuery({ eventId: id });
+
   const joinOrLeave = async () => {
     if (isUserParticipating) {
-      await leaveEvent({ eventId: id });
+      if (!payment) {
+        await leaveEvent({ eventId: id });
+      } else {
+        setShowLeaveModal(true);
+      }
     } else {
       try {
         await joinEvent({ eventId: id });
@@ -43,8 +51,60 @@ export const JoinOrLeaveEventButton: FunctionComponent<{
   };
 
   return (
-    <Button variant="outlined" color="primary" onClick={joinOrLeave}>
-      {isUserParticipating ? "Absagen" : "Zusagen"}
-    </Button>
+    <>
+      <Button variant="outlined" color="primary" onClick={joinOrLeave}>
+        {isUserParticipating ? "Absagen" : "Zusagen"}
+      </Button>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={showLeaveModal}
+        onClose={() => setShowLeaveModal(false)}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <ModalDialog
+          aria-labelledby="size-modal-title"
+          aria-describedby="size-modal-description"
+          className="max-w-xs md:max-w-2xl"
+        >
+          <ModalClose variant="outlined" className="rounded shadow-md" />
+          <Typography
+            component="h2"
+            id="modal-title"
+            level="h4"
+            textColor="inherit"
+            fontWeight="lg"
+            mb={1}
+            className="pr-10 pb-3"
+          >
+            Du hast bereits bezahlt, bist du dir sicher, dass du absagen willst?
+          </Typography>
+
+          <div className="flex justify-center gap-x-2">
+            <Button
+              variant="outlined"
+              color="info"
+              onClick={async () => {
+                await leaveEvent({ eventId: id });
+
+                setShowLeaveModal(false);
+              }}
+            >
+              Ja
+            </Button>
+
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => {
+                setShowLeaveModal(false);
+              }}
+            >
+              Nein
+            </Button>
+          </div>
+        </ModalDialog>
+      </Modal>
+    </>
   );
 };
