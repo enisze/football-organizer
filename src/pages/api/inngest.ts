b9ba65f2cdf71caaef3ecb9f9fbb622589aa7467
Enis <eniszej@gmail.com>
2022-11-29 23:29:25 +0100

    🐛

--- src/pages/api/inngest.ts
+++ src/pages/api/inngest.ts
@@ -1,7 +1,7 @@
 // ./pages/api/inngest.ts
 import { serve } from "inngest/next";
-import { sendNewEventEmail } from "./emails/sendNewEventEmail";
-import { sendPaymentAndEventReminder } from "./emails/sendPaymentAndEventReminderEmails";
+import { sendNewEventEmail } from "../../../inngest/sendNewEventEmail";
+import { sendPaymentAndEventReminder } from "../../../inngest/sendPaymentAndEventReminderEmails";
 export default serve("My App", [
   sendNewEventEmail,
   sendPaymentAndEventReminder,
