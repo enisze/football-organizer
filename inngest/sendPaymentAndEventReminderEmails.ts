c90ea9cf1d0f243557007b9f280ed4e9edfb7421
Enis <eniszej@gmail.com>
2022-12-02 20:52:47 +0100

    ⚗️

--- inngest/sendPaymentAndEventReminderEmails.ts
+++ inngest/sendPaymentAndEventReminderEmails.ts
@@ -1,5 +1,7 @@
 import { createFunction } from "inngest";
+import { find, forEach, reduce } from "lodash";
 import { PrismaClient } from "../prisma/generated/client";
+import { sendInBlueTransport } from "../src/emails/transporter";
 import type { Event__Reminder } from "./__generated__/types";
 
 const prisma = new PrismaClient();
@@ -10,99 +12,99 @@ const paypalLink =
 const job = async ({ event }: { event: Event__Reminder }) => {
   const id = event.data.eventId;
 
-  return { statusCode: 400, message: "test" };
+  if (!prisma)
+    return {
+      status: 400,
+      body: { message: "No prisma" },
+    };
 
-  //   if (!prisma)
-  //     return {
-  //       status: 400,
-  //       body: { message: "No prisma" },
-  //     };
+  let allUsers;
+  try {
+    allUsers = await prisma.user.findMany();
+  } catch (error: any) {
+    return {
+      status: 400,
+      body: { message: `No users ${error}` },
+    };
+  }
 
-  //   const allUsers = await prisma.user.findMany();
+  const footballEvent = await prisma.event.findUnique({
+    where: { id },
+    include: { participants: true, payments: true },
+  });
 
-  //   if (!allUsers)
-  //     return {
-  //       status: 400,
-  //       body: { message: "No users" },
-  //     };
+  if (!footballEvent)
+    return {
+      status: 400,
+      body: { message: "No football event" },
+    };
 
-  //   const footballEvent = await prisma.event.findUnique({
-  //     where: { id },
-  //     include: { participants: true, payments: true },
-  //   });
+  const { date, startTime, endTime, address, cost } = footballEvent;
 
-  //   if (!footballEvent)
-  //     return {
-  //       status: 400,
-  //       body: { message: "No football event" },
-  //     };
+  const participantIds = reduce(
+    footballEvent.participants,
+    (acc: string[], participant) => {
+      return [...acc, participant.id];
+    },
+    []
+  );
 
-  //   const { date, startTime, endTime, address, cost } = footballEvent;
+  if (!participantIds)
+    return {
+      status: 400,
+      body: { message: "No ids" },
+    };
 
-  //   const participantIds = reduce(
-  //     footballEvent.participants,
-  //     (acc: string[], participant) => {
-  //       return [...acc, participant.id];
-  //     },
-  //     []
-  //   );
+  forEach(allUsers, async (user) => {
+    if (!participantIds.includes(user.id)) {
+      //Send event reminder
+      await sendInBlueTransport.sendMail({
+        from: '"Sender Name" <eniszej@gmail.com>',
+        to: user.email,
+        subject: "ERINNERUNG: FUSSBALL FINDET STATT :) ",
+        html: `<p>Hey ${user.name},</p>
+          <p>Folgendes Event findet bald statt: </p>
+  <p>Datum: <strong>${date}</strong></p>
+  <p>Zeit: <strong>${startTime} - ${endTime} Uhr</strong></p>
+  <p>Ort: <strong>${address}</strong></p>
+  <p>Preis: <strong>${cost}</strong></p>
+  <a href="${paypalLink}">Hier kannst du bei Paypal bezahlen :)</a>
+  <p><strong>Sag doch bitte zu und komm vorbei :) </strong></p>
+          `,
+        headers: { "x-myheader": "test header" },
+      });
+    } else {
+      const payment = find(
+        footballEvent.payments,
+        (payment) => payment.userId === user.id
+      );
+      if (!payment) {
+        //Send payment reminder
+        await sendInBlueTransport.sendMail({
+          from: '"Sender Name" <eniszej@gmail.com>',
+          to: user.email,
+          subject: "ERINNERUNG: DU HAST FUSSBALL NOCH NICHT BEZAHLT :) ",
+          html: `<p>Hey ${user.name},</p>
+          <p>Du hast zu folgendem Event zugesagt: </p>
+  <p>Datum: <strong>${date}</strong></p>
+  <p>Zeit: <strong>${startTime} - ${endTime} Uhr</strong></p>
+  <p>Ort: <strong>${address}</strong></p>
+  <p>Preis: <strong>${cost}</strong></p>
 
-  //   if (!participantIds)
-  //     return {
-  //       status: 400,
-  //       body: { message: "No ids" },
-  //     };
+  <p><strong>Bezahl doch bitte ueber den unten stehenden Link bspw. :) Dankee </strong></p>
 
-  //   forEach(allUsers, async (user) => {
-  //     if (!participantIds.includes(user.id)) {
-  //       //Send event reminder
-  //       await sendInBlueTransport.sendMail({
-  //         from: '"Sender Name" <eniszej@gmail.com>',
-  //         to: user.email,
-  //         subject: "ERINNERUNG: FUSSBALL FINDET STATT :) ",
-  //         html: `<p>Hey ${user.name},</p>
-  //         <p>Folgendes Event findet bald statt: </p>
-  // <p>Datum: <strong>${date}</strong></p>
-  // <p>Zeit: <strong>${startTime} - ${endTime} Uhr</strong></p>
-  // <p>Ort: <strong>${address}</strong></p>
-  // <p>Preis: <strong>${cost}</strong></p>
-  // <a href="${paypalLink}">Hier kannst du bei Paypal bezahlen :)</a>
-  // <p><strong>Sag doch bitte zu und komm vorbei :) </strong></p>
-  //         `,
-  //         headers: { "x-myheader": "test header" },
-  //       });
-  //     } else {
-  //       const payment = find(
-  //         footballEvent.payments,
-  //         (payment) => payment.userId === user.id
-  //       );
-  //       if (!payment) {
-  //         //Send payment reminder
-  //         await sendInBlueTransport.sendMail({
-  //           from: '"Sender Name" <eniszej@gmail.com>',
-  //           to: user.email,
-  //           subject: "ERINNERUNG: DU HAST FUSSBALL NOCH NICHT BEZAHLT :) ",
-  //           html: `<p>Hey ${user.name},</p>
-  //         <p>Du hast zu folgendem Event zugesagt: </p>
-  // <p>Datum: <strong>${date}</strong></p>
-  // <p>Zeit: <strong>${startTime} - ${endTime} Uhr</strong></p>
-  // <p>Ort: <strong>${address}</strong></p>
-  // <p>Preis: <strong>${cost}</strong></p>
+  <a href="${paypalLink}">Hier kannst du bei Paypal bezahlen :)</a>
+          `,
+          headers: { "x-myheader": "test header" },
+        });
+      }
+    }
+  });
 
-  // <p><strong>Bezahl doch bitte ueber den unten stehenden Link bspw. :) Dankee </strong></p>
-
-  // <a href="${paypalLink}">Hier kannst du bei Paypal bezahlen :)</a>
-  //         `,
-  //           headers: { "x-myheader": "test header" },
-  //         });
-  //       }
-  //     }
-  //   });
-
-  //   return {
-  //     status: 200,
-  //     body: { message: "Done" },
-  //   };
+  return {
+    status: 200,
+    body: { message: "Done" },
+  };
 };
 export const sendPaymentAndEventReminder = createFunction(
   "Send Payment And Event Reminder",
