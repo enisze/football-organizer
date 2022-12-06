40e62d66b7d02eebb7ba0fb1af0bebb6dd777795
Enis <eniszej@gmail.com>
2022-12-06 19:26:21 +0100

    ⚗️

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 
+import { createSecureHeaders } from "next-secure-headers";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
@@ -10,6 +12,26 @@
 const config = {
   reactStrictMode: true,
   swcMinify: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: "'self'",
+              styleSrc: "self",
+            },
+          },
+          forceHTTPSRedirect: [
+            true,
+            { maxAge: 60 * 60 * 24 * 4, includeSubDomains: false },
+          ],
+          referrerPolicy: "same-origin",
+        }),
+      },
+    ];
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
