d22294a1c2a7999b536d8f3819ab9f425e6056d9
Enis <eniszej@gmail.com>
2022-12-10 19:46:14 +0100

    🐛

--- inngest/emailTemplates/helpers/getEventTemplate.tsx
+++ inngest/emailTemplates/helpers/getEventTemplate.tsx
@@ -1,4 +1,5 @@
 import type { Event } from "@prisma/client";
+import { transformDate } from "../../../src/helpers/transformDate";
 
 export const getEventTemplate = (event: Partial<Event>) => {
   const { address, cost, date, endTime, startTime } = event;
@@ -10,7 +11,9 @@ export const getEventTemplate = (event: Partial<Event>) => {
       </mj-column>
       <mj-column>
         <mj-text align="center" color="#FFF" font-size="15px" font-family="Ubuntu, Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px" padding-bottom="0px"><strong>Datum</strong></mj-text>
-        <mj-text align="center" color="#FFF" font-size="13px" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="20px" padding-top="10px">${date}</mj-text>
+        <mj-text align="center" color="#FFF" font-size="13px" font-family="Helvetica" padding-left="25px" padding-right="25px" padding-bottom="20px" padding-top="10px">${
+          date ? transformDate(date) : "Unbekannt"
+        }</mj-text>
       </mj-column>
       <mj-column>
         <mj-text align="center" color="#FFF" font-size="15px" font-family="Ubuntu, Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px" padding-bottom="0px"><strong>Uhrzeit</strong></mj-text>
