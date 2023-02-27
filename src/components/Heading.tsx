a81d3e70ac6c7c6788d40e52979682824099f50b
Enis <eniszej@gmail.com>
2023-02-27 22:04:46 +0100

    💄

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,23 +1,25 @@
-import Link from "next/link";
-import type { FunctionComponent } from "react";
+import Link from 'next/link'
+import type { FunctionComponent } from 'react'
 
 export const Heading: FunctionComponent<{
-  size?: "sm" | "md" | "lg";
-}> = ({ size = "lg" }) => {
+  size?: 'sm' | 'md' | 'lg'
+}> = ({ size = 'lg' }) => {
   const fontSize =
-    size === "lg"
-      ? "text-[80px]"
-      : size === "md"
-      ? "text-[48px]"
-      : "text-[20px]";
+    size === 'lg'
+      ? 'text-[80px]'
+      : size === 'md'
+      ? 'text-[48px]'
+      : 'text-[20px]'
   return (
-    <Link href={process.env.NEXT_PUBLIC_BASE_URL ?? ""}>
-      <span
-        className={`cursor-pointer text-center font-extrabold leading-normal ${fontSize} bg-gradient-to-r from-blue-400 to-blue-200 bg-clip-text text-transparent dark:from-blue-800 dark:to-slate-800`}
-      >
-        Football
-        <span className="text-slate-900 dark:text-slate-100"> Organizer</span>
-      </span>
+    <Link href={process.env.NEXT_PUBLIC_BASE_URL ?? ''}>
+      <div className="text-center">
+        <span
+          className={`cursor-pointer font-extrabold leading-normal ${fontSize} bg-gradient-to-r from-blue-400 to-blue-200 bg-clip-text text-transparent dark:from-blue-800 dark:to-slate-800`}
+        >
+          Football
+          <span className="text-slate-900 dark:text-slate-100"> Organizer</span>
+        </span>
+      </div>
     </Link>
-  );
-};
+  )
+}
