efff69ff3f178715978c5bf32b709276b52f9b8a
Enis <eniszej@gmail.com>
2023-02-27 22:49:37 +0100

    🐛

--- src/components/Events/Buttons/JoinEventButton.tsx
+++ src/components/Events/Buttons/JoinEventButton.tsx
@@ -1,37 +1,37 @@
-import { trpc } from "@/src/utils/trpc";
-import { Button } from "@/ui/base/Button";
-import { TRPCError } from "@trpc/server";
-import type { FunctionComponent } from "react";
-import { LoadingWrapper } from "../../LoadingWrapper";
+import { trpc } from '@/src/utils/trpc'
+import { Button } from '@/ui/base/Button'
+import { TRPCError } from '@trpc/server'
+import type { FunctionComponent } from 'react'
+import { LoadingWrapper } from '../../LoadingWrapper'
 
 export const JoinEventButton: FunctionComponent<{
-  id: string;
+  id: string
 }> = ({ id }) => {
-  const trpcContext = trpc.useContext();
+  const trpcContext = trpc.useContext()
 
   const { mutateAsync: joinEvent, isLoading: loadingJoin } =
     trpc.event.join.useMutation({
       onSuccess: () => {
-        trpcContext.invalidate();
+        trpcContext.invalidate()
       },
-    });
+    })
 
   const join = async () => {
     try {
-      await joinEvent({ eventId: id });
+      await joinEvent({ eventId: id })
     } catch (error) {
       if (error instanceof TRPCError) {
-        error.code === "PRECONDITION_FAILED";
+        error.code === 'PRECONDITION_FAILED'
       }
-      alert("Leider ist kein Platz mehr frei :( ");
+      alert('Leider ist kein Platz mehr frei :( ')
     }
-  };
+  }
 
   return (
-    <LoadingWrapper isLoading={loadingJoin} className="self-center">
+    <LoadingWrapper isLoading={loadingJoin}>
       <Button variant="outline" onClick={join} className="w-full">
         Zusagen
       </Button>
     </LoadingWrapper>
-  );
-};
+  )
+}
