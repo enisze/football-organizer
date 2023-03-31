511d5da88dc3d08c269923ddde061579bb82a79a
Enis <eniszej@gmail.com>
2023-03-31 22:33:09 +0200

    ♻️

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -11,6 +11,7 @@ import { useSession } from 'next-auth/react'
 import type { FunctionComponent } from 'react'
 import { useState } from 'react'
 import { LoginForm } from '../Authentication/LoginForm'
+import { GroupSelector } from '../Groups/GroupSelector'
 import { Heading } from '../Heading'
 import { OrganizerMenu } from './OrganizerMenu'
 
@@ -23,6 +24,8 @@ export const Navbar: FunctionComponent = () => {
     <header className="sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white dark:border-b-slate-700 dark:bg-slate-900">
       <nav className="flex items-center justify-between px-2 py-3">
         <Heading size="sm" />
+
+        <GroupSelector />
 
         <div className="flex gap-x-1">
           <div className={`flex cursor-pointer items-center`}>
