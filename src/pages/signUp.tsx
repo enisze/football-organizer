98a881712bc591c77d669e019a1807731b232fa0
Enis <eniszej@gmail.com>
2023-04-02 22:34:44 +0200

    🐛

--- src/pages/signUp.tsx
+++
@@ -1,9 +0,0 @@
-import type { FunctionComponent } from 'react'
-
-import { SignUpForm } from '../components/SignUpForm'
-
-const SignUp: FunctionComponent = () => {
-  return <SignUpForm />
-}
-
-export default SignUp
