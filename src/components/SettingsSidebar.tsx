4d48f65ea27147911340e2de4e3fe1df15619ac7
Enis <eniszej@gmail.com>
2023-04-02 23:33:37 +0200

    💄

--- src/components/SettingsSidebar.tsx
+++ src/components/SettingsSidebar.tsx
@@ -17,7 +17,7 @@ export const SettingsSidebar: FunctionComponent = () => {
         <div className="relative flex">
           <span>Accounteinstellungen </span>
 
-          {!hasPaypalName && <NotificationBubble className="-right-3" />}
+          {!hasPaypalName && <NotificationBubble className="!-right-3" />}
         </div>
       </OrganizerLink>
       <OrganizerLink href={'/settings/groups'} className={''}>
