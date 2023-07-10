549caf952190b5f39a203ca994d3d398ebd17999
Enis <eniszej@gmail.com>
2023-07-10 23:28:09 +0200

    ♻️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,16 @@
 import { type NextPage } from 'next'
-import { useSession } from 'next-auth/react'
 import { Hero } from '../components/Heading'
-import { LoadingWrapper } from '../components/LoadingWrapper'
 import { Navbar } from '../components/Navigation/Navbar'
 
 const Home: NextPage = () => {
-  const { status } = useSession()
-  const style = status === 'loading' ? 'grid place-items-center' : ''
-
   return (
-    <div className={`${style} h-full`}>
+    <div className="h-full">
       <title>Event Wizard</title>
-      <LoadingWrapper isLoading={status === 'loading'}>
-        <Navbar />
-        <main className="absolute flex h-full w-full flex-col items-center justify-center">
-          <Hero />
-          {/* <ContactForm /> */}
-        </main>
-      </LoadingWrapper>
+      <Navbar />
+      <main className="absolute flex h-full w-full flex-col items-center justify-center">
+        <Hero />
+        {/* <ContactForm /> */}
+      </main>
     </div>
   )
 }
