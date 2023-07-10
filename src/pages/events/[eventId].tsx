2f6fd9562314c6afad9a986c70f46729630ebb96
Enis <eniszej@gmail.com>
2023-07-11 01:41:58 +0200

    🐛

--- src/pages/events/[eventId].tsx
+++ src/pages/events/[eventId].tsx
@@ -4,15 +4,19 @@ import Link from 'next/link'
 import type { FunctionComponent } from 'react'
 import { prisma } from '../../../prisma/prisma'
 
+import { Event } from '@/prisma/generated/client'
 import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from 'next'
 import { getServerSession } from 'next-auth'
+import SuperJSON from 'superjson'
 import { trpc } from '../../utils/trpc'
 import { authOptions } from '../api/auth/[...nextauth]'
 
-const getServerSideProps = async (context: GetServerSidePropsContext) => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
   const id = context.params?.eventId as string
 
   const session = await getServerSession(context.req, context.res, authOptions)
@@ -40,7 +44,7 @@ const getServerSideProps = async (context: GetServerSidePropsContext) => {
 
   return {
     props: {
-      event: event,
+      event: SuperJSON.serialize(event),
     },
   }
 }
@@ -49,6 +53,8 @@ const EventPage: FunctionComponent<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ event }) => {
   const trpcContext = trpc.useContext()
+
+  const deserializedEvent: Event = SuperJSON.deserialize(event)
 
   const { mutate: setStatus, isSuccess } =
     trpc.event.setParticipatingStatus.useMutation({
@@ -62,39 +68,30 @@ const EventPage: FunctionComponent<
   const link = new URL(url)
 
   return (
-    <>
-      <div
-        style={{
-          background: 'linear-gradient(to top, #373B44, #73C8A9)',
-        }}
-        className="fixed -z-10 flex h-full w-full"
-      />
+    <div className="mx-20 flex flex-col">
+      <div className="flex flex-col items-center">
+        <div className="my-5 flex flex-col items-center justify-center gap-y-2 rounded p-5">
+          <Button
+            onClick={() =>
+              setStatus({ eventId: deserializedEvent.id, status: 'CANCELED' })
+            }
+            variant="outline"
+          >
+            Keine Emails mehr erhalten
+          </Button>
 
-      <div className="mx-20 flex flex-col">
-        <div className="flex flex-col items-center">
-          <div className="my-5 flex flex-col items-center justify-center gap-y-2 rounded p-5">
-            <Button
-              onClick={() =>
-                setStatus({ eventId: event.id, status: 'CANCELED' })
-              }
-              variant="outline"
-            >
-              Keine Emails mehr erhalten
-            </Button>
-
-            {isSuccess && (
-              <span className="text-green-500">
-                Du hast dich erfolgreich abgemeldet.
-              </span>
-            )}
-            <Link href={link}>
-              <span>Zur Startseite</span>
-            </Link>
-          </div>
-          <EventCard event={event} />
+          {isSuccess && (
+            <span className="text-green-500">
+              Du hast dich erfolgreich abgemeldet.
+            </span>
+          )}
+          <Link href={link}>
+            <span>Zur Startseite</span>
+          </Link>
         </div>
+        <EventCard event={deserializedEvent} />
       </div>
-    </>
+    </div>
   )
 }
 
