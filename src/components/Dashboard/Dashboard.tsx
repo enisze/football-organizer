ab60bb48039e01cb92273d2489e0bbf636ddc490
Enis <eniszej@gmail.com>
2023-07-11 00:01:41 +0200

    ♻️

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,9 @@
 import type { FunctionComponent } from 'react'
 
 import type { Event } from '@/prisma/generated/client'
+import { useIsAdmin } from '@/src/hooks/useIsAdmin'
 import { OrganizerLink } from '@/ui/base/OrganizerLink'
+import { addDays } from 'date-fns'
 import { EventCard } from '../Events/EventCard'
 import { GroupSelector } from '../Groups/GroupSelector'
 
@@ -9,6 +11,7 @@ export const Dashboard: FunctionComponent<{
   events?: Event[]
   groupNames?: string[]
 }> = ({ events, groupNames }) => {
+  const isAdmin = useIsAdmin()
   return (
     <div className="m-8 flex flex-col gap-y-3 justify-center items-center">
       {groupNames && groupNames.length > 0 ? (
@@ -17,6 +20,8 @@ export const Dashboard: FunctionComponent<{
           <ul className="flex flex-col gap-y-2">
             {events && events?.length > 0 ? (
               events.map((event) => {
+                if (addDays(event.date, 1) < new Date() && !isAdmin) return null
+
                 return (
                   <li key={event.id}>
                     <EventCard event={event} />
