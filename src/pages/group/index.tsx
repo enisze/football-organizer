59910f8e8415992c830e756b70b115347a21e599
Enis <eniszej@gmail.com>
2023-07-23 23:50:30 +0200

    ♻️

--- src/pages/group/index.tsx
+++ src/pages/group/index.tsx
@@ -1,14 +1,7 @@
-import { Dashboard } from '@/src/components/Dashboard/Dashboard'
 import Navbar from '@/src/components/Navigation/Navbar'
 import { OrganizerLink } from '@/ui/OrganizerLink'
-import type {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-} from 'next'
+import type { GetServerSidePropsContext } from 'next'
 import { getServerSession } from 'next-auth'
-import { useRouter } from 'next/router'
-import type { FunctionComponent } from 'react'
-import { useEffect, useRef } from 'react'
 import { prisma } from '../../../prisma/prisma'
 import { authOptions } from '../api/auth/[...nextauth]'
 
@@ -22,41 +15,27 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
 
   const id = group.at(0)?.id
 
-  return {
-    props: {
-      id,
-    },
+  if (id) {
+    return {
+      redirect: {
+        destination: `/group/${id}`,
+      },
+    }
   }
+  return { props: {} }
 }
 
-const MainPage: FunctionComponent<
-  InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ id }) => {
-  const isInitialGroupSet = useRef(false)
-
-  const router = useRouter()
-
-  useEffect(() => {
-    if (isInitialGroupSet.current) return
-    isInitialGroupSet.current = true
-    router.push(`/group/${id}`)
-  }, [id, router])
-
+const MainPage = () => {
   return (
     <div className="flex flex-col pb-2">
       <Navbar />
-      <div className="p-8" />
-      {id ? (
-        <Dashboard />
-      ) : (
-        <div className="flex flex-col justify-center">
-          <span>Du bist noch kein Mitglied einer Gruppe</span>
+      <div className="flex flex-col justify-center">
+        <span>Du bist noch kein Mitglied einer Gruppe</span>
 
-          <OrganizerLink href="/settings/groups" className="justify-center">
-            Grupper erstellen
-          </OrganizerLink>
-        </div>
-      )}
+        <OrganizerLink href="/settings/groups" className="justify-center">
+          Grupper erstellen
+        </OrganizerLink>
+      </div>
     </div>
   )
 }
