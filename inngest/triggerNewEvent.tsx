0d00d246c6a4d215d96aa94c292b67b1b2e5a209
Enis <eniszej@gmail.com>
2023-10-04 00:23:20 +0200

    ⚗️

--- inngest/triggerNewEvent.tsx
+++ inngest/triggerNewEvent.tsx
@@ -5,36 +5,33 @@ export const triggerNewEvent = inngest.createFunction(
   { name: 'Trigger New Event Email' },
   { event: 'event/new' },
   async ({ step, event: inngestEvent, prisma }) => {
-    await step.run('test', async () => {
-      const eventId = inngestEvent.data.id
+    const eventId = inngestEvent.data.id
 
+    const event = await step.run('get event', async () => {
+      console.log('getting event')
       const event = await prisma.event.findUnique({
         where: { id: eventId },
         include: { participants: true },
       })
 
-      console.log('test')
+      return event
+    })
 
-      if (!event) return { message: `No event found ${eventId}` }
+    if (!event) return { message: `No event found ${eventId}` }
 
-      if (!event.groupId) return { message: `No group found ${event.groupId}` }
+    if (!event.groupId) return { message: `No group found ${event.groupId}` }
 
+    const usersOfGroup = await step.run('get users', async () => {
+      console.log('getting users')
       try {
         const usersOnGroup = await prisma.group.findUnique({
           where: {
-            id: event.groupId,
+            id: event.groupId ?? undefined,
           },
           select: { users: true },
         })
 
-        if (!usersOnGroup)
-          return {
-            message: `No users found`,
-          }
-
-        const usersWhoGotMails: string[] = []
-
-        const days = differenceInCalendarDays(new Date(event.date), new Date())
+        if (!usersOnGroup) return []
 
         const usersOfGroup = await Promise.all(
           usersOnGroup.users.map(async (user) => {
@@ -42,29 +39,38 @@ export const triggerNewEvent = inngest.createFunction(
           }),
         )
 
-        usersOfGroup
-          .filter((user) => user?.notificationsEnabled)
-          .forEach(async (user) => {
-            if (!user) return
-
-            await inngest.send({
-              name: 'event/newEmail',
-              data: {
-                user,
-                id: event.id,
-                days,
-              },
-            })
-
-            usersWhoGotMails.push(user.email)
-          })
-
-        return { success: true }
+        return usersOfGroup
       } catch (error: unknown) {
-        return {
-          message: `No users ${error}`,
-        }
+        console.log(error)
       }
     })
+
+    if (!usersOfGroup) return { message: `No users found` }
+
+    await step.run('sending newEmail event', async () => {
+      console.log('sending event')
+      const usersWhoGotMails: string[] = []
+
+      const days = differenceInCalendarDays(new Date(event.date), new Date())
+
+      usersOfGroup
+        .filter((user) => user?.notificationsEnabled)
+        .forEach(async (user) => {
+          if (!user) return
+
+          await inngest.send({
+            name: 'event/newEmail',
+            data: {
+              user,
+              id: event.id,
+              days,
+            },
+          })
+
+          usersWhoGotMails.push(user.email)
+        })
+    })
+
+    return { success: true }
   },
 )
