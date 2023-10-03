146b1a393677352a55e17f2c3a84ca9e441bc952
Enis <eniszej@gmail.com>
2023-10-04 00:52:41 +0200

    ⚗️

--- inngest/sendEventReminderEmail.tsx
+++ inngest/sendEventReminderEmail.tsx
@@ -8,7 +8,7 @@ export const sendEventReminderEmail = inngest.createFunction(
   { name: 'Send Event Reminder Email' },
   { event: 'event/reminderEmail' },
 
-  async ({ event: inngestEvent, prisma }) => {
+  async ({ event: inngestEvent, prisma, step, logger }) => {
     const id = inngestEvent.data.id as string
 
     const user = inngestEvent.data.user as {
@@ -18,33 +18,53 @@ export const sendEventReminderEmail = inngest.createFunction(
 
     const participantsAmount = inngestEvent.data.participantsAmount as number
 
-    const event = await prisma.event.findUnique({
-      where: { id },
-      include: { participants: true },
-    })
+    const event = await step.run(
+      'get event',
+      async () =>
+        await prisma.event.findUnique({
+          where: { id },
+          include: { participants: true },
+        }),
+    )
 
     if (!event) return
 
     const html = render(
       <EventReminder
-        event={event}
+        event={{
+          ...event,
+          date: new Date(event.date),
+          bookingDate: event.bookingDate ? new Date(event.bookingDate) : null,
+          createdAt: new Date(event.createdAt),
+          updatedAt: new Date(event.updatedAt),
+        }}
         userName={user.name}
         participantsAmount={participantsAmount}
       />,
     )
 
-    const days = differenceInCalendarDays(event.date, new Date())
+    const days = differenceInCalendarDays(new Date(event.date), new Date())
 
-    const { response } = await sendEmail(
-      user.email,
-      html,
-      `Erinnerung: Fussball in ${days} Tagen, ${participantsAmount}/${event.maxParticipants} Teilnehmer!`,
-    )
+    const { response } = await step.run('sending mail', async () => {
+      try {
+        const response = await sendEmail(
+          user.email,
+          html,
+          `Erinnerung: Fussball in ${days} Tagen, ${participantsAmount}/${event.maxParticipants} Teilnehmer!`,
+        )
 
-    console.log(
-      `Message sent to: ${JSON.stringify(user.email)}, Code : ${
-        response.statusCode
-      }`,
+        return response
+      } catch (error: unknown) {
+        console.log(error)
+
+        return { response: null }
+      }
+    })
+
+    logger.info(
+      `Message sent to: ${JSON.stringify(
+        user.email,
+      )}, Code : ${response?.statusCode}`,
     )
   },
 )
