196ffbdeec65d4a69c1ac17540fe4df46d0c6ad5
Enis <eniszej@gmail.com>
2023-10-04 00:10:07 +0200

    ⚗️

--- inngest/triggerNewEvent.tsx
+++ inngest/triggerNewEvent.tsx
@@ -4,63 +4,67 @@ import { differenceInCalendarDays } from 'date-fns'
 export const triggerNewEvent = inngest.createFunction(
   { name: 'Trigger New Event Email' },
   { event: 'event/new' },
-  async ({ event: inngestEvent, prisma }) => {
-    const eventId = inngestEvent.data.id
+  async ({ step, event: inngestEvent, prisma }) => {
+    await step.run('test', async () => {
+      const eventId = inngestEvent.data.id
 
-    const event = await prisma.event.findUnique({
-      where: { id: eventId },
-      include: { participants: true },
-    })
-
-    if (!event) return { message: `No event found ${eventId}` }
-
-    if (!event.groupId) return { message: `No group found ${event.groupId}` }
-
-    try {
-      const usersOnGroup = await prisma.group.findUnique({
-        where: {
-          id: event.groupId,
-        },
-        select: { users: true },
+      const event = await prisma.event.findUnique({
+        where: { id: eventId },
+        include: { participants: true },
       })
 
-      if (!usersOnGroup)
-        return {
-          message: `No users found`,
-        }
+      console.log('test')
 
-      const usersWhoGotMails: string[] = []
+      if (!event) return { message: `No event found ${eventId}` }
 
-      const days = differenceInCalendarDays(new Date(event.date), new Date())
+      if (!event.groupId) return { message: `No group found ${event.groupId}` }
 
-      const usersOfGroup = await Promise.all(
-        usersOnGroup.users.map(async (user) => {
-          return await prisma.user.findUnique({ where: { id: user.id } })
-        }),
-      )
-
-      usersOfGroup
-        .filter((user) => user?.notificationsEnabled)
-        .forEach(async (user) => {
-          if (!user) return
-
-          await inngest.send({
-            name: 'event/newEmail',
-            data: {
-              user,
-              id: event.id,
-              days,
-            },
-          })
-
-          usersWhoGotMails.push(user.email)
+      try {
+        const usersOnGroup = await prisma.group.findUnique({
+          where: {
+            id: event.groupId,
+          },
+          select: { users: true },
         })
 
-      return { success: true }
-    } catch (error: unknown) {
-      return {
-        message: `No users ${error}`,
+        if (!usersOnGroup)
+          return {
+            message: `No users found`,
+          }
+
+        const usersWhoGotMails: string[] = []
+
+        const days = differenceInCalendarDays(new Date(event.date), new Date())
+
+        const usersOfGroup = await Promise.all(
+          usersOnGroup.users.map(async (user) => {
+            return await prisma.user.findUnique({ where: { id: user.id } })
+          }),
+        )
+
+        usersOfGroup
+          .filter((user) => user?.notificationsEnabled)
+          .forEach(async (user) => {
+            if (!user) return
+
+            await inngest.send({
+              name: 'event/newEmail',
+              data: {
+                user,
+                id: event.id,
+                days,
+              },
+            })
+
+            usersWhoGotMails.push(user.email)
+          })
+
+        return { success: true }
+      } catch (error: unknown) {
+        return {
+          message: `No users ${error}`,
+        }
       }
-    }
+    })
   },
 )
